fe78a1608495c2e047a86370c44f40672dfb0022
Faith Chau <tszsumchau97@gmail.com>
2018-11-15 13:59:00 -0800

    created relations and defined schemas

---
+++ src/restaurantReservation.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE RestaurantReservation;
+USE RestaurantReservation;
+
+CREATE TABLE Customer(
+  cID INT AUTO_INCREMENT,
+  name VARCHAR(50),
+  phoneNum (int),
+  primary key(cID)
+);
+
+CREATE TABLE Employee(
+  sID INT AUTO_INCREMENT,
+  name VARCHAR(50),
+  isOff BOOLEAN,
+  primary key(sID)
+);
+
+CREATE TABLE Restaurant(
+  tID INT AUTO_INCREMENT,
+  occupied BOOLEAN DEFAULT FALSE,
+  numOfSeats INT,
+  sID INT,
+  subServerID INT,
+  primary key(tID),
+  FOREIGN KEY(sID) REFERENCES Employee(sID) on update cascade,
+  FOREIGN KEY(subServerID) REFERENCES Employee(sID) on update cascade,
+);
+
+CREATE TABLE Reservations(
+  numOfTable INT,
+  timeReserved VARCHAR(50),
+  cID INT,
+  primary key (cID, numOfTable),
+  FOREIGN KEY (cID) REFERENCES Customer(cID) on update cascade
+);
+
+CREATE TABLE CurrentDropIns(
+  numOfTable INT,
+  timeDropIn VARCHAR(50),
+  cID INT,
+  queueID INT,
+  primary key (cID, numOfTable),
+  FOREIGN KEY (cID) REFERENCES Customer(cID) on update cascade
+); 
